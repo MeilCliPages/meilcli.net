b0986b41b045c6d0939f6a419ef795eb4db729c0
MeilCli <ics.sora.meil@gmail.com>
2024-07-28 09:05:16 +0900

    add analytics id

--- src/constant.ts
+++ src/constant.ts
@@ -5,4 +5,4 @@ export const siteAuthor = "meil";
 export const siteStartYear = 2019;
 export const twitter = "penguin_sharp";
 export const github = "MeilCli";
-export const analyticsId = "";
+export const analyticsId = "G-7SGSPE1PG1";
